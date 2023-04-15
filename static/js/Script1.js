5b34ad1a7e48c9ff2e05eba4e86b970520dffafb
alicia fernandez <fernandez.m.alicia2020@gmail.com>
2023-04-15 19:40:12 -0400

    added requirements.txt

--- static/js/Script1.js
+++ static/js/Script1.js
@@ -37,3 +37,120 @@ async function addNewCupcake() {
             console.log(error);
         });
 }
+
+
+
+//FEATURES FOR ICON BAR -- SIZE UP, CHANGE COLOR,  AND ADD ANIMATION ON HOVER//
+$(".species-icon").hover(
+    function () {
+        $(this).addClass("fa-lg");
+        $(this).addClass("fa-beat");
+        $(this).addClass("text-primary");
+        /*$(this).addClass("pop_form");*/
+        /*$("#pop_form").append(miniform);*/
+    }, function () {
+        $(this).removeClass("fa-lg");
+        $(this).removeClass("fa-beat");
+        $(this).removeClass("text-primary");
+        /*$("#miniform").remove();*/
+    }
+);
+
+$(".github-icon").hover(
+    function () {
+        $(this).addClass("fa-lg");
+        $(this).addClass("fa-beat");
+        $(this).addClass("text-primary");
+        /*$(this).addClass("pop_form");*/
+        /*$("#pop_form").append(miniform);*/
+    }, function () {
+        $(this).removeClass("fa-lg");
+        $(this).removeClass("fa-beat");
+        $(this).removeClass("text-primary");
+        /*$("#miniform").remove();*/
+    }
+);
+
+$(".reqs-icon").hover(
+    function () {
+        $(this).addClass("fa-lg");
+        $(this).addClass("fa-beat");
+        $(this).addClass("text-primary");
+        /*$(this).addClass("pop_form");*/
+        /*$("#pop_form").append(miniform);*/
+    }, function () {
+        $(this).removeClass("fa-lg");
+        $(this).removeClass("fa-beat");
+        $(this).removeClass("text-primary");
+        /*$("#miniform").remove();*/
+    }
+);
+
+
+
+$(".notes-icon").hover(
+    function () {
+        $(this).addClass("fa-lg");
+        $(this).addClass("fa-beat");
+        $(this).addClass("text-primary");
+    }, function () {
+        $(this).removeClass("fa-lg");
+        $(this).removeClass("fa-beat");
+        $(this).removeClass("text-primary");
+    }
+);
+
+$(".edit-icon").hover(
+    function () {
+        $(this).addClass("fa-lg");
+        $(this).addClass("fa-beat");
+        $(this).addClass("text-primary");
+    }, function () {
+        $(this).removeClass("fa-lg");
+        $(this).removeClass("fa-beat");
+        $(this).removeClass("text-primary");
+    }
+);
+
+$(".bday-icon").hover(
+    function () {
+        $(this).addClass("fa-lg");
+        $(this).addClass("fa-beat");
+        $(this).addClass("text-primary");
+    }, function () {
+        $(this).removeClass("fa-lg");
+        $(this).removeClass("fa-beat");
+        $(this).removeClass("text-primary");
+    }
+);
+
+
+
+let home = 'https://github.com/Fern-Ali/';
+let repo = 'https://github.com/Fern-Ali/CupcakeDataBase';
+let reqs = 'https://github.com/Fern-Ali/Pet-Adoption-App/blob/master/requirements.txt';
+
+//$(".edit-icon").on("click", addPetPage);
+//$(".species-icon").on("click", editPetPage);
+$(".github-icon").on("click", { event_type: home }, goToGithub);
+$(".project-icon").on("click", { event_type: repo }, goToGithub);
+$(".reqs-icon").on("click", { event_type: reqs }, goToGithub);
+
+
+
+function addPetPage() {
+    window.location.href = '/add';
+    return false;
+}
+
+function editPetPage() {
+    let id = $(this).attr('id');
+    window.location.href = `/pets/${id}/edit`;
+    return false;
+}
+
+function goToGithub(event) {
+
+    window.location.href = event.data.event_type;
+    return false;
+}
